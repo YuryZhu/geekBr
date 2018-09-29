9a8a0e92361bd5de5459e12e574dbc8fbbe6bd82
yury <zhurikk@ya.ru>
2018-09-29 23:07:26 +0300

    no message

--- src/main/java/ru/yzhiharevich/geekb/App.java
+++ src/main/java/ru/yzhiharevich/geekb/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         Course c = new Course(); // Создаем полосу препятствий
         Team team = new Team(); // Создаем команду
-        team.showTeamMates("Pobeda");
+        team.showTeamMates("Lubov");
         c.doIt(); // Просим команду пройти полосу
     }
 }
